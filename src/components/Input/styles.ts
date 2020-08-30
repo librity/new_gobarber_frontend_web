27e8c4a76fc7b616288a7275ea4f4be298d4778b
Luis Geniole <luis.geniole@gmail.com>
2020-08-30 16:00:21 -0300

    Add fill and focus styling to inputs.

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
   padding: 16px;
@@ -8,8 +13,22 @@ export const Container = styled.div`
 
   background: #232129;
   border-radius: 10px;
+
   border: 2px solid #232129;
   color: #666360;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   & + div {
     margin-top: 8px;
